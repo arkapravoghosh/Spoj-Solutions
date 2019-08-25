f10e356da46bc911b00fe5b08c78282107a32de3
Arkapravo Ghosh <arkapravoghosh98@gmail.com>
2019-08-25 14:00:36 +0530

    Update CAM5.cpp

--- CAM5.cpp
+++ CAM5.cpp
@@ -1,66 +1,45 @@
-// Problem link - https://www.spoj.com/problems/CAM5/
+// www.spoj.com/problems/CAM5/
 
 #include <bits/stdc++.h>
 using namespace std;
-
-#define ll long long
-#define ull unsigned long long
-#define ld long double
+ 
 #define pb push_back
-#define ppb pop_back
-#define pii pair<ll, ll>
-#define vi vector<ll>
-#define vull vector<ull>
-#define vpii vector<pii >
-#define mt make_tuple
-#define ff first
-#define ss second
-#define uset unordered_set
-#define umap unordered_map
-#define all(x) x.begin(), x.end()
-#define revall(x) x.rbegin(), x.rend()
-#define rep(i, j, k) for(ll i = j; i < k; ++i)
 #define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 #define T int tt; cin>>tt; while(tt--)
-
-const ll MOD = (ll)(1e9+7);
-const int inf = (int)INFINITY;
-const ll INF = (ll)INFINITY;
-const int MAX = (int)(1e5+1);
-
-int parent[MAX], size[MAX];
-
-int find(int u) {
-    return u == parent[u] ? u : find(parent[u]);
+ 
+const int MAX = (int)1e5+1;
+ 
+vector<int> G[MAX];
+bool vis[MAX] = {false};
+ 
+void dfs(int v) {
+    vis[v] = true;
+    for(auto u : G[v])
+        if(!vis[u])
+            dfs(u);
 }
-
-void merge(int u, int v) {
-    u = find(u);
-    v = find(v);
-    if(u == v) return;
-    if(size[u] < size[v]) swap(u, v);
-    parent[v] = u;
-    size[u] += size[v];
-}
-
+ 
 int main() {
     fastio;
     T {
         int n, m;
-        cin >> n >> m;
-        for(int i = 0; i < n; ++i) {
-            parent[i] = i;
-            size[i] = 1;
-        }
+        cin>>n>>m;
         for(int i = 0; i < m; ++i) {
             int u, v;
             cin >> u >> v;
-            merge(u, v);
+            G[u].pb(v);
+            G[v].pb(u);
         }
-        uset<int> res;
-        for(int i = 0; i < n; ++i)
-            res.insert(parent[i]);
-        cout << res.size() << '\n';
+        int comp = 0;
+        for(int i = 0; i < n; ++i) {
+            if(!vis[i]) {
+                comp++;
+                dfs(i);
+            }
+        }
+        cout << comp << '\n';
+        for(int i = 0; i <= n; ++i)
+            G[i].clear(), vis[i] = false;
     }
     return 0;
-}
+} 
